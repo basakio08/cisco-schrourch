2966b98fbf3fafa9ecc22959a99bd2e6d0cfd548
Schrouch <ugobendotti@me.com>
2017-05-18 22:16:26 +0200

    testt

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("TG BALTRiNGUE");
-        Personne unePersonne = new Personne();
-        unePersonne.nom = "Cisco";
-        unePersonne.prenom = "ccna";
-        unePersonne.direBonjour();
-        System.out.println(unePersonne.nom);
-    }
-
 
 }
